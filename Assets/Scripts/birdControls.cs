bd335da94e4c234b20a866d8b4277490b0af1459
Anthony Broussard <anbroussard@csumb.edu>
2024-06-14 17:54:41 -0700

    Control and Env Updates
    Made some minor updates to the bird control and environment prefabs based on our previous discussions.

--- Assets/Scripts/birdControls.cs
+++ Assets/Scripts/birdControls.cs
@@ -1,27 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class birdControls : MonoBehaviour
 {
-    Rigidbody m_Rigidbody;
-    public float moveSpeed;
-    public float jumpSpeed;
+    Rigidbody2D m_Rigidbody;
+    public float jumpSpeed = 1.5f;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        m_Rigidbody = GetComponent<Rigidbody>();
+        m_Rigidbody = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        m_Rigidbody.velocity = transform.right * moveSpeed;
-
         if(Input.GetKey(KeyCode.Space)){
-            m_Rigidbody.AddForce(Vector2.up * jumpSpeed);
+            m_Rigidbody.velocity = Vector2.up * jumpSpeed;
 
         }
     }
