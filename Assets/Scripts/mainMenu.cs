7e9dbe8a07e74d996f89959319f9b998df024d6e
Anthony Broussard <anbroussard@csumb.edu>
2024-07-21 16:02:46 -0700

    Main Menu Updates
    Connected all buttons in the main menu. Options and How To windows now open/close. Game can be quit via options.

--- Assets/Scripts/mainMenu.cs
+++ Assets/Scripts/mainMenu.cs
@@ -4,12 +4,41 @@ using UnityEngine.SceneManagement;
 
 public class mainMenu : MonoBehaviour
 {
+    [SerializeField] GameObject mainWindow;
+    [SerializeField] GameObject optionsWindow;
+    [SerializeField] GameObject howToWindow;
     [SerializeField] GameObject vfxLeft;
     [SerializeField] GameObject vfxRight;
+    
     public GameObject hoverPanel;
 
     public void PlayGame(){
         StartCoroutine(WaitCoroutine());
+    }
+    public void ShowMain(){
+        optionsWindow.SetActive(false);
+        howToWindow.SetActive(false);
+        mainWindow.SetActive(true);
+    }
+
+    public void ShowOptions(){
+        mainWindow.SetActive(false);
+        optionsWindow.SetActive(true);
+        
+    }
+
+    public void ShowHowto(){
+        mainWindow.SetActive(false);
+        optionsWindow.SetActive(false);
+        howToWindow.SetActive(true);
+    }
+
+    public void Quit(){
+        // Use for actual build
+        //Application.Quit();
+
+        // Use for testing in editor
+        UnityEditor.EditorApplication.isPlaying = false;
     }
 
     public void TriggerVFX(){
