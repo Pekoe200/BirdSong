e154e9c1761a39df41ec6cb0fa8ecca33fc6d4f6
Anthony Broussard <anbroussard@csumb.edu>
2024-07-21 14:53:44 -0700

    UI Particles
    Added some UI particle effects for when the user clicks the play button. This particle system and code can be used to add an effect for leaves flying in the levels.

--- Assets/Scripts/mainMenu.cs
+++ Assets/Scripts/mainMenu.cs
@@ -1,11 +1,25 @@
 using UnityEngine;
+using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class mainMenu : MonoBehaviour
 {
+    [SerializeField] GameObject vfxLeft;
+    [SerializeField] GameObject vfxRight;
     public GameObject hoverPanel;
 
     public void PlayGame(){
+        StartCoroutine(WaitCoroutine());
+    }
+
+    public void TriggerVFX(){
+        vfxLeft.SetActive(true);
+        vfxRight.SetActive(true);
+    }
+
+    IEnumerator WaitCoroutine()
+    {
+        yield return new WaitForSecondsRealtime(1);
         SceneManager.LoadSceneAsync(1);
     }
 }
