bacc78334760ee559ec3cc81a4b29554bde0619f
Anthony Broussard <anbroussard@csumb.edu>
2024-08-09 18:42:08 -0700

    Fixed Transition for lvl 1 to lvl 2
    Fixed transition. Unable to fix level 3

--- Assets/Scripts/sceneTrigger.cs
+++ Assets/Scripts/sceneTrigger.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class sceneTrigger : MonoBehaviour
 {
     public sceneTransition sceneTransition;
     public string sceneName;
+    public int sceneNo;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            sceneTransition.FadeToScene(sceneName);
+            //sceneTransition.FadeToScene(sceneName);
+            SceneManager.LoadSceneAsync(sceneNo);
         }
     }
 
